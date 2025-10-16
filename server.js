d106aa8b2b3f32062f430d51b9bce47004bf21be
ivanmunoz <iivanmunoz@hotmail.com>
2025-10-15 18:20:12 -0600

    update Api registrar

--- server.js
+++ server.js
@@ -324,7 +324,7 @@ app.post('/registrar', async (req, res) => {
     }
     
     try {
-        const query = 'INSERT INTO clientes (nombre, email, mensaje) VALUES (?, ?, ?)';
+        const query = 'INSERT INTO clientes (nombre, correo_electronico, mensaje) VALUES (?, ?, ?)';
         const [result] = await pool.execute(query, [nombre, email, mensaje]);
         
         console.log('--- Contacto Guardado ---');
